337c280022d8d36cc3becd759e50c8708108d2a0
Anuj <anuj.chaudhary10@gmail.com>
2018-03-28 00:11:49 +0530

    test

---
+++ src/com/practice/producer/consumer/Consumer.java
@@ -0,0 +1,24 @@
+package com.practice.producer.consumer;
+
+public class Consumer extends Thread {
+
+	private final MarketPlace m;
+
+	public Consumer(MarketPlace m) {
+		super();
+		this.m = m;
+	}
+
+	@Override
+	public void run() {
+		for(int i = 0;i < 10 ;i++){
+			try {
+				m.consume();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	}
+
---
+++ src/com/practice/trees/TreeFindGivenData.java
@@ -0,0 +1,42 @@
+package com.practice.trees;
+
+public class TreeFindGivenData {
+
+	public static void main(String[] args) {
+		Node n = new Node(5);
+		n.left = new Node(4);
+		n.left.right = new Node(3);
+		n.left.left = new Node(6);
+		n.right = new Node(19);
+		n.right.left = new Node(10);
+		n.right.right = new Node(9);
+
+		System.out.println("check : " + traverse(n, 100, false));
+
+	}
+
+	static boolean traverse(Node node, int data, boolean gotIt) {
+		if (node == null) {
+			return false;
+		}
+		if (gotIt) {
+			return true;
+		}
+		if (node.data == data) {
+			System.out.println(true);
+			return true;
+		}
+		gotIt = traverse(node.left, data, gotIt);
+		System.out.println(node.data);
+		gotIt = traverse(node.right, data, gotIt);
+		return gotIt;
+	}
+
+}
+ class Node {
+	public Node(int data){
+		this.data = data;
+	}
+int data;
+Node left,right;
+}
