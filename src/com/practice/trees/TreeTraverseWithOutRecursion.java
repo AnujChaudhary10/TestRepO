b08626fc5368d8b04924aa539e3f414543bcdc46
Anuj <anuj.chaudhary10@gmail.com>
2018-03-28 21:35:50 +0530

    Removed wrong implementation of PostOrder by traversal

--- src/com/practice/trees/TreeTraverseWithOutRecursion.java
+++ src/com/practice/trees/TreeTraverseWithOutRecursion.java
@@ -13,9 +13,28 @@ import java.util.Stack;
 public class TreeTraverseWithOutRecursion {
 
 	/**
-	 * @param args
+	 * (a) Inorder (Left, Root, Right) (b) Preorder (Root, Left, Right) (c)
+	 * Postorder (Left, Right, Root)
 	 */
 	public static void main(String[] args) {
+
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		traverseInorder(root);
+		System.out.println("inOrder");
+		printInorder(root);
+		System.out.println("inOrder");
+		printPostorder(root);
+		System.out.println("postOrder");
+		traversePostOrder(root);
+		System.out.println("postOrder");
+		printPreorder(root);
+		System.out.println("preOrder");
+		traversePreOrder(root);
+
 		Node n = new Node(5);
 		n.left = new Node(4);
 		n.left.right = new Node(3);
@@ -25,15 +44,17 @@ public class TreeTraverseWithOutRecursion {
 		n.right = new Node(19);
 		n.right.left = new Node(10);
 		n.right.right = new Node(9);
-		traverseInorder(n);
+	/*	traverseInorder(n);
 		System.out.println("inOrder");
 		printInorder(n);
 		System.out.println("inOrder");
 		printPostorder(n);
 		System.out.println("postOrder");
+		traversePostOrder(n);
+		System.out.println("postOrder");
 		printPreorder(n);
 		System.out.println("preOrder");
-		traversePostOrder(n);
+		traversePreOrder(n);*/
 	}
 
 	static void traverseInorder(Node node) {
@@ -58,46 +79,23 @@ public class TreeTraverseWithOutRecursion {
 		}
 
 	}
-	static void traversePostOrder(Node node) {
-		if (Objects.isNull(node))
-			return;
 
-		Stack<Node> leftNodes = new Stack<>();
-		while (node != null) {
-			leftNodes.push(node); // push all left node
-			node = node.left;
-		}
-		while (!leftNodes.isEmpty()) {
-			Node n = leftNodes.pop();
-			if (Objects.nonNull(n.right)) {
-				n = n.right;
-				while (n != null) {
-					leftNodes.push(n);
-					n = n.left;
-				}
-			}
-			
-		}
+	static void traversePostOrder(Node node) {}
 
-	}
 	static void traversePreOrder(Node node) {
 		if (Objects.isNull(node))
 			return;
 
-		Stack<Node> leftNodes = new Stack<>();
-		while (node != null) {
-			leftNodes.push(node); // push all left node
-			node = node.left;
-		}
-		while (!leftNodes.isEmpty()) {
-			Node n = leftNodes.pop();
-			if (Objects.nonNull(n.right)) {
-				n = n.right;
-				while (n != null) {
-					leftNodes.push(n);
-					n = n.left;
-				}
-			}
+		Stack<Node> nodes = new Stack<>();
+		nodes.push(node);
+
+		while (!nodes.isEmpty()) {
+			Node n = nodes.pop();
+			System.out.print(n.data + " ");
+			if (n.right != null)
+				nodes.push(n.right);
+			if (n.left != null)
+				nodes.push(n.left);
 		}
 
 	}
